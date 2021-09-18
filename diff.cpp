aa360495c1c4d781bf777d14397a2e9489cb890b
vedantmodi2020 <vedantmodi201834@gmail.com>
2021-09-18 13:08:54 +0530

    adding diff function

---
+++ diff.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    int x,y;
+    cout<<"Enter two Numbers";
+    cin>>x;
+    cin>>y;
+    x=x-y;
+    cout<<"the difference is"<<x;
+
+    return 0;
+}
